9a577cdc82e812270972a297b5d4c67369504ebf
zoupengcheng <443813032@qq.com>
2026-09-22 09:57:04 +0800

    删除bin/obj

--- obj/Debug/net10.0/customersupport.AssemblyInfo.cs
+++ obj/Debug/net10.0/customersupport.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("customersupport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a938d0e02c6cc9a77adac33970d2ce76275078e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("customersupport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("customersupport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
